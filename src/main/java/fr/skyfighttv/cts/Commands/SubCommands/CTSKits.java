a44d8f56b959ac40eb35e20b576e8b4fa1ff7ca8
SKyfightTV <antoinecraft.fr@gmail.com>
2021-01-14 23:08:54 +0100

    [add] CTSKits

--- src/main/java/fr/skyfighttv/cts/Commands/SubCommands/CTSKits.java
+++ src/main/java/fr/skyfighttv/cts/Commands/SubCommands/CTSKits.java
@@ -1,4 +1,45 @@
 package fr.skyfighttv.cts.Commands.SubCommands;
 
+import fr.ChadOW.cinventory.cinventory.CInventory;
+import fr.ChadOW.cinventory.citem.CItem;
+import fr.ChadOW.cinventory.citem.ItemCreator;
+import fr.skyfighttv.cts.Utils.FileManager;
+import fr.skyfighttv.cts.Utils.Files;
+import fr.skyfighttv.cts.Utils.PlayersManager;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
+
 public class CTSKits {
+    public static void init(Player player) {
+        YamlConfiguration kitsConfig = FileManager.getValues().get(Files.Kits);
+
+        CInventory inventory = new CInventory(27, "Kit menu");
+
+        for (String kits : kitsConfig.getKeys(false)) {
+            CItem item = new CItem(new ItemCreator(Material.getMaterial(kitsConfig.getString(kits + ".ItemMenu")), 0)
+                    .setName(kits)
+                    .setLores(kitsConfig.getStringList(kits + ".Lore")));
+            item.addEvent((cInventory, cItem, player1, clickContext) -> {
+                if (kitsConfig.getKeys(false).contains(cItem.getName())) {
+                    if (kitsConfig.getString(cItem.getName() + ".Permission").equals("N/A")
+                            || player.hasPermission(kitsConfig.getString(cItem.getName() + ".Permission"))) {
+                        try {
+                            PlayersManager.setKit(player, cItem.getName());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        player.sendMessage(FileManager.getValues().get(Files.Lang).getString("SuccessSelectKit"));
+
+                        inventory.close(player);
+                    }
+                }
+            });
+            inventory.addElement(item);
+        }
+        inventory.open(player);
+    }
 }
