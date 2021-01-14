a70ab1c5516c4450ec937e68bec13feb3a8145b2
SKyfightTV <antoinecraft.fr@gmail.com>
2021-01-14 23:05:51 +0100

    [add] CTSSetKit

--- src/main/java/fr/skyfighttv/cts/Commands/SubCommands/CTSSetKit.java
+++ src/main/java/fr/skyfighttv/cts/Commands/SubCommands/CTSSetKit.java
@@ -1,4 +1,32 @@
 package fr.skyfighttv.cts.Commands.SubCommands;
 
+import fr.skyfighttv.cts.Utils.FileManager;
+import fr.skyfighttv.cts.Utils.Files;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CTSSetKit {
+    public static void init(Player player, String kit) {
+        YamlConfiguration kitsConfig = FileManager.getValues().get(Files.Kits);
+        YamlConfiguration langConfig = FileManager.getValues().get(Files.Lang);
+
+        List<ItemStack> content = new ArrayList<>(Arrays.asList(player.getInventory().getContents()));
+        for (int i = 0; i < 4; i++)
+            content.remove(content.size() - 1);
+
+        kitsConfig.set(kit + ".Content", content);
+        kitsConfig.set(kit + ".ArmorContent", player.getInventory().getArmorContents());
+        kitsConfig.set(kit + ".ItemMenu", "DIAMOND_SWORD");
+        kitsConfig.set(kit + ".Lore", new ArrayList<>());
+        kitsConfig.set(kit + ".Permission", "N/A");
+
+        FileManager.save(Files.Kits);
+
+        player.sendMessage(langConfig.getString("SuccessSetKit"));
+    }
 }
