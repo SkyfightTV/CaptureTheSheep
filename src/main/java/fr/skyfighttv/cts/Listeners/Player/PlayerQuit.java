6c85f1701eb3523a28ae8cec6d973fad2f8ce190
SkyfightTV <antoinecraft.fr@gmail.com>
2021-01-17 16:17:24 +0100

    [add] Event PlayerQuit

---
+++ src/main/java/fr/skyfighttv/cts/Listeners/Player/PlayerQuit.java
@@ -0,0 +1,26 @@
+package fr.skyfighttv.cts.Listeners.Player;
+
+import fr.skyfighttv.cts.Commands.CTS;
+import fr.skyfighttv.cts.Utils.GameManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerQuit implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        CTS.inGamePlayers.remove(player);
+
+        GameManager.getNumberPlayers().get(event.getPlayer().getWorld()).remove(player);
+        GameManager.getRedTeam().get(event.getPlayer().getWorld()).remove(player);
+        GameManager.getBlueTeam().get(event.getPlayer().getWorld()).remove(player);
+
+        player.getInventory().clear();
+        player.getInventory().setArmorContents(new ItemStack[0]);
+    }
+}
