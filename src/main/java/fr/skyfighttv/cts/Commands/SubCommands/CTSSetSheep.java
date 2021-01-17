c3394c7156c25fdeb9c92b01d5c6130e58628dd8
SkyfightTV <antoinecraft.fr@gmail.com>
2021-01-17 01:14:30 +0100

    [fix] CTSSetSheep.java

--- src/main/java/fr/skyfighttv/cts/Commands/SubCommands/CTSSetSheep.java
+++ src/main/java/fr/skyfighttv/cts/Commands/SubCommands/CTSSetSheep.java
@@ -7,16 +7,16 @@ import org.bukkit.entity.Player;
 
 public class CTSSetSheep {
     public static void init(Player player, String sheep) {
-        YamlConfiguration spawnConfig = FileManager.getValues().get(Files.Spawn);
+        YamlConfiguration sheepConfig = FileManager.getValues().get(Files.Sheep);
         YamlConfiguration langConfig = FileManager.getValues().get(Files.Lang);
 
         if (sheep.equalsIgnoreCase("Blue")) {
-            spawnConfig.set("BlueSheep", player.getLocation().getBlock().getLocation());
+            sheepConfig.set("Blue", player.getLocation().getBlock().getLocation());
             FileManager.save(Files.Spawn);
 
             player.sendMessage(langConfig.getString("SuccessSetSheep"));
         } else if (sheep.equalsIgnoreCase("Red")) {
-            spawnConfig.set("RedSheep", player.getLocation().getBlock().getLocation());
+            sheepConfig.set("Red", player.getLocation().getBlock().getLocation());
             FileManager.save(Files.Spawn);
 
             player.sendMessage(langConfig.getString("SuccessSetSheep"));
