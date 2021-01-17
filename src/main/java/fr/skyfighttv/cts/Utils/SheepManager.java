e814a1f9f47c8f88cc458016c00feb0da8f94f46
SkyfightTV <antoinecraft.fr@gmail.com>
2021-01-17 22:33:59 +0100

    [update] SheepManager.java

--- src/main/java/fr/skyfighttv/cts/Utils/SheepManager.java
+++ src/main/java/fr/skyfighttv/cts/Utils/SheepManager.java
@@ -1,15 +1,19 @@
 package fr.skyfighttv.cts.Utils;
 
+import fr.skyfighttv.cts.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Sheep;
 
 import java.util.HashMap;
 
 public class SheepManager {
     private static final HashMap<World, HashMap<String, Sheep>> sheepGames = new HashMap<>();
+    private static final HashMap<Player, Integer> sheedPassengerId = new HashMap<>();
 
     public static void create(World world, String sheepTeam) {
         YamlConfiguration sheepConfig = FileManager.getValues().get(Files.Sheep);
@@ -30,6 +34,48 @@ public class SheepManager {
             sheepGames.get(world).put(sheepTeam, sheep);
         } catch (NullPointerException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void setPassenger(Player player, Sheep sheep) {
+        World world = player.getWorld();
+
+        if (sheepGames.containsKey(world)
+                && sheepGames.get(world).containsValue(sheep)) {
+
+            String sheepTeam;
+
+            if (sheepGames.get(world).get("Blue").equals(sheep))
+                sheepTeam = "Blue";
+            else if (sheepGames.get(world).get("Red").equals(sheep))
+                sheepTeam = "Red";
+            else return;
+
+            if ((sheepTeam.equals("Blue")
+                    && GameManager.getBlueTeam().get(world).contains(player))
+                    || (sheepTeam.equals("Red")
+                    && GameManager.getRedTeam().get(world).contains(player)))
+                return;
+
+            if (sheep.getPassenger() == null) {
+                sheep.setPassenger(player);
+
+                sheedPassengerId.put(player, Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getInstance(), () -> {
+                    if (sheep.getPassenger() == null) {
+                        YamlConfiguration sheepConfig = FileManager.getValues().get(Files.Sheep);
+
+                        Location sheepSpawn = new Location(world
+                                , ((Location) sheepConfig.get(sheepTeam)).getX()
+                                , ((Location) sheepConfig.get(sheepTeam)).getY()
+                                , ((Location) sheepConfig.get(sheepTeam)).getZ());
+
+                        sheep.teleport(sheepSpawn);
+
+                        Bukkit.getScheduler().cancelTask(sheedPassengerId.get(player));
+                        sheedPassengerId.remove(player);
+                    }
+                }, 0, 10));
+            }
         }
     }
 
