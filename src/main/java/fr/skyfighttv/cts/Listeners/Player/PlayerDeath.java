7cb1057e35bf4f27d3bc875b4a721193914d21bf
SkyfightTV <antoinecraft.fr@gmail.com>
2021-01-18 16:26:31 +0100

    [update] Event PlayerDeath

--- src/main/java/fr/skyfighttv/cts/Listeners/Player/PlayerDeath.java
+++ src/main/java/fr/skyfighttv/cts/Listeners/Player/PlayerDeath.java
@@ -1,8 +1,12 @@
 package fr.skyfighttv.cts.Listeners.Player;
 
 import fr.skyfighttv.cts.Commands.CTS;
+import fr.skyfighttv.cts.Main;
+import fr.skyfighttv.cts.Utils.FileManager;
+import fr.skyfighttv.cts.Utils.Files;
 import fr.skyfighttv.cts.Utils.GameManager;
 import fr.skyfighttv.cts.Utils.PlayersManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -23,8 +27,13 @@ public class PlayerDeath implements Listener {
             PlayersManager.addDeath(player, 1);
 
             player.spigot().respawn();
+
             GameManager.teleportPlayerTeam(event.getEntity().getWorld(), player);
             GameManager.givePlayerKit(player);
+
+            CTS.invinciblePlayers.add(player);
+
+            Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> CTS.invinciblePlayers.remove(player), FileManager.getValues().get(Files.Config).getInt("Game.Invincibility"));
         }
     }
 }
