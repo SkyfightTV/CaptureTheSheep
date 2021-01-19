2ef6bbf68a79464e6784eb3aa11b63341344bae1
SkyfightTV <antoinecraft.fr@gmail.com>
2021-01-19 21:32:30 +0100

    [update] Event PlayerJoin

--- src/main/java/fr/skyfighttv/cts/Listeners/Player/PlayerJoin.java
+++ src/main/java/fr/skyfighttv/cts/Listeners/Player/PlayerJoin.java
@@ -1,6 +1,7 @@
 package fr.skyfighttv.cts.Listeners.Player;
 
 import fr.skyfighttv.cts.Commands.CTS;
+import fr.skyfighttv.cts.Commands.SubCommands.CTSSetup;
 import fr.skyfighttv.cts.Utils.FileManager;
 import fr.skyfighttv.cts.Utils.Files;
 import fr.skyfighttv.cts.Utils.PlayersManager;
@@ -15,17 +16,26 @@ import java.io.IOException;
 public class PlayerJoin implements Listener {
     @EventHandler
     private void onJoin(PlayerJoinEvent event) {
-        try {
-            PlayersManager.create(event.getPlayer());
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (FileManager.getValues().get(Files.Config).getBoolean("IsSetup")) {
+            try {
+                PlayersManager.create(event.getPlayer());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            YamlConfiguration spawnConfig = FileManager.getValues().get(Files.Spawn);
+
+            if (spawnConfig.contains("Lobby"))
+                event.getPlayer().teleport((Location) spawnConfig.get("Lobby"));
+
+            CTS.setLobbyInventory(event.getPlayer());
+        } else {
+            if (!event.getPlayer().hasPermission("CTS.setup")) {
+                event.getPlayer().sendMessage(FileManager.getValues().get(Files.Lang).getString("NoPermission"));
+                return;
+            }
+
+            CTSSetup.init(event.getPlayer());
         }
-
-        YamlConfiguration spawnConfig = FileManager.getValues().get(Files.Spawn);
-
-        if (spawnConfig.contains("Lobby"))
-            event.getPlayer().teleport((Location) spawnConfig.get("Lobby"));
-
-        CTS.setLobbyInventory(event.getPlayer());
     }
 }
