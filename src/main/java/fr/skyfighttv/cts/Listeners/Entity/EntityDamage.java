648c2d0d3f5cd5e537de361994f086858a51ec50
SkyfightTV <antoinecraft.fr@gmail.com>
2021-01-25 18:55:49 +0100

    [update] EntityDamage.java

--- src/main/java/fr/skyfighttv/cts/Listeners/Entity/EntityDamage.java
+++ src/main/java/fr/skyfighttv/cts/Listeners/Entity/EntityDamage.java
@@ -2,7 +2,6 @@ package fr.skyfighttv.cts.Listeners.Entity;
 
 import fr.skyfighttv.cts.Utils.GameManager;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -11,9 +10,7 @@ public class EntityDamage implements Listener {
     @EventHandler
     private void onEntityDamage(EntityDamageEvent event) {
         if (GameManager.getNumberPlayers().containsKey(event.getEntity().getWorld())) {
-            if (event.getEntity() instanceof Sheep) {
-                event.setCancelled(true);
-            } else if (event.getEntity() instanceof Player
+            if (event.getEntity() instanceof Player
                     && GameManager.getInGamePlayers().contains(event.getEntity())
                     && event.getCause() == EntityDamageEvent.DamageCause.FALL
                     || !GameManager.getGames().contains(event.getEntity().getWorld())
